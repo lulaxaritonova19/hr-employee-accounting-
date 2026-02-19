e5d9c7606287ef1d6407fc8aefbc9072c9b692c0
lulaxaritonova19 <ula998322@gmail.com>
2026-02-19 20:44:02 +0300

    chore: remove .gitkeep (replaced with actual files)

--- server/server.js
+++ server/server.js
@@ -2,8 +2,20 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+// Подключаем middleware для парсинга JSON
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// Отдаем статические файлы из папки public
+app.use(express.static('public'))
+
+// Подключаем маршруты для сотрудников
+const employeeRoutes = require('./routes/employees')
+app.use('/api/employees', employeeRoutes)
+
+// Главная страница
 app.get('/', (req, res) => {
-	res.send('HR Server is running')
+	res.sendFile(__dirname + '/public/index.html')
 })
 
 app.listen(PORT, () => {
